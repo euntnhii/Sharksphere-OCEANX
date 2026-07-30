bce539a68dbb07c638569adfb37eda8aa626eae4
chiaj <288535992+euntnhii@users.noreply.github.com>
2026-07-30 12:02:04 +0800

    Configure frontend to use deployed backend API

--- src/api/anomalyService.ts
+++ src/api/anomalyService.ts
@@ -4,19 +4,21 @@ import type { AnomalyResult } from "../types/ecosystemState";
 
 export async function getAnomalyResult(ecosystemState: any): Promise<AnomalyResult> {
 
-    const response = await fetch("http://localhost:8000/predict", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            Apex_Predators: ecosystemState.populations.apexPredator,
-            Herbivore_Scrapers: ecosystemState.populations.herbivoreScraper,
-            Turf_Brushers: ecosystemState.populations.turfBrusher,
-            Invertebrate_Prey_Hunters: ecosystemState.populations.invertebrateHunter,
-            Small_Invertebrates: ecosystemState.populations.smallInvertebrate,
-        }),
-    });
+    const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/predict`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                Apex_Predators: ecosystemState.populations.apexPredator,
+                Herbivore_Scrapers: ecosystemState.populations.herbivoreScraper,
+                Turf_Brushers: ecosystemState.populations.turfBrusher,
+                Invertebrate_Prey_Hunters: ecosystemState.populations.invertebrateHunter,
+                Small_Invertebrates: ecosystemState.populations.smallInvertebrate,
+            }),
+        });
 
     const result = await response.json();
     console.log(result);
